a26b2e373249c98349b3fdebf1f00e20f142f780
Vaishnav Bhat <104012712+vaishnavbhat05@users.noreply.github.com>
2024-05-05 22:42:54 +0530

    Create InstanceOf_Operator.java

---
+++ InstanceOf_Operator.java
@@ -0,0 +1,12 @@
+// Demonstration of InstanceOf Operator.//
+public class Test {
+
+   public static void main(String args[]) {
+
+      String name = "James";
+
+      // following will return true since name is type of String
+      boolean result = name instanceof String;
+      System.out.println( result );
+   }
+}
